096221ee133a6d9514321fc8f76f515a3f91a0f6
Jacek Krużel <jacek.kruzel@gmail.com>
2016-12-06 22:48:46 +0100

    Structure change

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { SimpleTreeNavigator, SimpleTreeNode } from './simple-tree-navigator.service'
-export { SimpleTreeNavigatorsProvider } from './simple-tree-navigator-provider.service'
-export { SimpleTreeModule } from './simple-tree.module'
