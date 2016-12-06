664a945175e0eb2719014858921966dc9b07c143
Jacek Krużel <jacek.kruzel@gmail.com>
2016-12-06 22:16:43 +0100

    Package structure change

--- index.ts
+++
@@ -1,3 +0,0 @@
-export { SimpleTreeNavigator, SimpleTreeNode } from './src/simple-tree-navigator.service'
-export { SimpleTreeNavigatorsProvider } from './src/simple-tree-navigator-provider.service'
-export { SimpleTreeModule } from './src/simple-tree.module'
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { SimpleTreeNavigator, SimpleTreeNode } from './simple-tree-navigator.service'
+export { SimpleTreeNavigatorsProvider } from './simple-tree-navigator-provider.service'
+export { SimpleTreeModule } from './simple-tree.module'
