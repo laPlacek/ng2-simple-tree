5849dbf512a1f13fa6f4b8682df4ebab16719396
Jacek Krużel <jacek.kruzel@gmail.com>
2016-12-08 02:02:19 +0100

    Dependencies clean-up.

--- src/simple-tree-navigator.service.ts
+++ src/simple-tree-navigator.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import isUndefined = require('lodash/isUndefined');
 
 export interface NodeObservables {
   isCurrentNode: Observable<boolean>;
